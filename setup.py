01f6f8994a9e89832fab93917e5911aa8320f1a0
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2023-12-20 08:52:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,3 +58,296 @@ MPESA Daraja API documentation can be found at https://developer.safaricom.co.ke
     'Source': 'https://github.com/martinmogusu/django-daraja',
 },
 )
+#####Another way###
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-daraja',
+    version='1.2.0',
+    description='A Python Django library for interacting with the Safaricom MPESA Daraja API.',
+    long_description=read('README.md'),
+    author='Martin Mogusu',
+    author_email='martinmogusu@gmail.com',
+    url='https://github.com/martinmogusu/django-daraja',
+    download_url='https://github.com/martinmogusu/django-daraja.git',
+    license='MIT License',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.11',
+        'python-decouple',
+        'requests',
+        'cryptography'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    zip_safe=False,
+    test_suite='tests.runtests.start',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='mpesa django daraja finance mobile-money safaricom api',
+    project_urls={
+        'Documentation': 'https://django-daraja.readthedocs.io/',
+        'Source': 'https://github.com/martinmogusu/django-daraja',
+    },
+)
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-daraja',
+    version='1.2.0',
+    description='A Python Django library for interacting with the Safaricom MPESA Daraja API.',
+    long_description=read('README.md'),
+    author='Martin Mogusu',
+    author_email='martinmogusu@gmail.com',
+    url='https://github.com/martinmogusu/django-daraja',
+    download_url='https://github.com/martinmogusu/django-daraja.git',
+    license='MIT License',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.11',
+        'python-decouple',
+        'requests',
+        'cryptography'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    zip_safe=False,
+    test_suite='tests.runtests.start',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='mpesa django daraja finance mobile-money safaricom api',
+    project_urls={
+        'Documentation': 'https://django-daraja.readthedocs.io/',
+        'Source': 'https://github.com/martinmogusu/django-daraja',
+    },
+)
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-daraja',
+    version='1.2.0',
+    description='A Python Django library for interacting with the Safaricom MPESA Daraja API.',
+    long_description=read('README.md'),
+    author='Martin Mogusu',
+    author_email='martinmogusu@gmail.com',
+    url='https://github.com/martinmogusu/django-daraja',
+    download_url='https://github.com/martinmogusu/django-daraja.git',
+    license='MIT License',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.11',
+        'python-decouple',
+        'requests',
+        'cryptography'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    zip_safe=False,
+    test_suite='tests.runtests.start',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='mpesa django daraja finance mobile-money safaricom api',
+    project_urls={
+        'Documentation': 'https://django-daraja.readthedocs.io/',
+        'Source': 'https://github.com/martinmogusu/django-daraja',
+    },
+)
+
+############another way to do it##############
+import os
+from setuptools import setup, find_packages
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+setup(
+    name='django-daraja',
+    version='1.2.0',
+    description='A Python Django library for interacting with the Safaricom MPESA Daraja API.',
+    long_description=read_file('README.md'),
+    author='Martin Mogusu',
+    author_email='martinmogusu@gmail.com',
+    url='https://github.com/martinmogusu/django-daraja',
+    download_url='https://github.com/martinmogusu/django-daraja.git',
+    license='MIT License',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.11',
+        'python-decouple',
+        'requests',
+        'cryptography'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    zip_safe=False,
+    test_suite='tests.runtests.start',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='mpesa django daraja finance mobile-money safaricom api',
+    project_urls={
+        'Documentation': 'https://django-daraja.readthedocs.io/',
+        'Source': 'https://github.com/martinmogusu/django-daraja',
+    },
+)
+
+
+####way 2#####
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-daraja',
+    version='1.2.0',
+    description='A Python Django library for interacting with the Safaricom MPESA Daraja API.',
+    long_description=read('README.md'),
+    author='Martin Mogusu',
+    author_email='martinmogusu@gmail.com',
+    url='https://github.com/martinmogusu/django-daraja',
+    download_url='https://github.com/martinmogusu/django-daraja.git',
+    license='MIT License',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.11',
+        'python-decouple',
+        'requests',
+        'cryptography'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    zip_safe=False,
+    test_suite='tests.runtests.start',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='mpesa django daraja finance mobile-money safaricom api',
+    project_urls={
+        'Documentation': 'https://django-daraja.readthedocs.io/',
+        'Source': 'https://github.com/martinmogusu/django-daraja',
+    },
+)
+Version 2:
+python
+Copy code
+import os
+from setuptools import setup, find_packages
+
+def read_file(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as file:
+        return file.read()
+
+setup(
+    name='django-daraja',
+    version='1.2.0',
+    description='A Python Django library for interacting with the Safaricom MPESA Daraja API.',
+    long_description=read_file('README.md'),
+    author='Martin Mogusu',
+    author_email='martinmogusu@gmail.com',
+    url='https://github.com/martinmogusu/django-daraja',
+    download_url='https://github.com/martinmogusu/django-daraja.git',
+    license='MIT License',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.11',
+        'python-decouple',
+        'requests',
+        'cryptography'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ],
+    zip_safe=False,
+    test_suite='tests.runtests.start',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='mpesa django daraja finance mobile-money safaricom api',
+    project_urls={
+        'Documentation': 'https://django-daraja.readthedocs.io/',
+        'Source': 'https://github.com/martinmogusu/django-daraja',
+    },
+)
